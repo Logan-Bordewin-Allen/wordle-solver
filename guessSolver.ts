9692e871c0ea331da23612c79b7bb527c5a6bd50
Logan Bordewin-Allen <275417011+Logan-Bordewin-Allen@users.noreply.github.com>
2026-05-31 11:45:43 -0600

    made it to changes

--- guessSolver.ts
+++ guessSolver.ts
@@ -1,15 +1,17 @@
 // NEED TO REFACTOR THIS INTO A "CLASS" TO USE TEMP ONES FOR SOLVER
 // DO THIS BEFORE ANYTHING ELSE
 
+//OK I DID IT ARE YOU HAPPY
+
 
 // Array containing every word that the answer could still be
 let possibleWords: String[]
+//Dictionary array
+let dictionary: String[]
 // 5 rows representing the 5 letter word, 26 columns representing alphabet
 // 0 means no info, 1 means letter goes in this position
 // 2 means letter can't go in this position, 3 means letter is in the word and could be in this position
 let knownInfo:  number[][]
-// collection of every yellow letter
-let neededLetters: number[]
 
 
 
@@ -18,9 +20,10 @@ let neededLetters: number[]
 export function initialize(words: String[]){
     // possibleWords reset
     possibleWords = words
-    // knownInfo reset
+    dictionary = words
     // Probably a better way to do this
     knownInfo = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -28,49 +31,80 @@ export function initialize(words: String[]){
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     ]
 
-    neededLetters = []
 }
 
 
-// infoUpdate helper function use updated knownInfo array to remove all immpossible words 
-function removeWords(){
-    // loop thorugh current possibleWords
-    for(let i = 0; i < possibleWords.length; i++){
-        let isPossible = 0
+export function takeGuess(guessInfo:number[], guess:String){
+    knownInfo = infoUpdate(guessInfo,guess,knownInfo)
+}
 
+
+function toArray(guess: String, answer: String): number[]{
+    let guessArr = []
+    let answerArr = []
+    let output = []
+    for(let i = 0; i < 5;i++){
+        guessArr.push(guess.substring(i,i+1) as unknown as number - ("a" as unknown as number))
+        answerArr.push(answer.substring(i,i+1) as unknown as number - ("a" as unknown as number))
+    }
+    for(let i = 0; i < 5;i++){
+        if(guessArr[i] == answerArr[i]){
+            output.push(1)
+        } else if (guessArr[i] in answerArr){
+            output.push(3)
+        } else{
+            output.push(2)
+        }
+    }
+    return output
+}
+
+
+// infoUpdate helper function use updated knownInfo array to remove all impossible words 
+function removeWords(pos: String[], info: number[][]): String[]{
+    
+    // loop thorugh current possibleWords
+    for(let i = 0; i < pos.length; i++){
+        let letters = []
         // loop through each letter of the possible word
         for(let j = 0; j < 5; j++){
             // get letter at j as a number
-            let letter = possibleWords[i].substring(j,j + 1) as unknown as number -("a" as unknown as number)
+            let letter = pos[i].substring(j,j + 1) as unknown as number -("a" as unknown as number)
             // remove letter if knownInfo position is 2
             if(knownInfo[j][letter] == 2){
-                possibleWords.splice(i,1)
+                pos.splice(i,1)
                 i--;
             }
-            //increment isPossible if position is 3
-            if(knownInfo[j][letter] == 3){
-                isPossible++
-            }
+            letters.push(letter);
         }
         // if the word does not contain enough possible letters, remove it
-        // todo: edge case error where word contains multiple letters but not every letter in neededletters
-        // i. e. neededletters = [p,i], apple will not be deleted
-        if(isPossible < neededLetters.length){
-            possibleWords.splice(i,1)
+        for(let i = 0; i < info[6].length;i++){  
+            if(info[6][i] = 1){
+                for(let j = 0; j < letters.length; j++){
+                    if(letters[j] = i){
+                        letters.splice(j,1)
+                        break
+                    }
+                }
+            }
+        }
+        if(letters.length != 0){
+            pos.splice(i,1)
             i--
         }
     }
+    return pos
 }
 
 
 
 
-// Takes guess and updates knownInfo, same numbers as knownInfo
+// Takes guess and outputs new array, same numbers as knownInfo
 // If word is crane and guess is creak info should be [1,1,3,3,2]
-export function infoUpdate(info: number[], guess: String){
+function infoUpdate(info: number[], guess: String, knownInfoArr: number[][]): number[][]{
     // words.csv has Capitalized letters that can mess with math
     guess.toLowerCase()
-
+    let output = knownInfoArr
     // todo make more efficent
     for(let i = 0; i < 5; i++){
         // gets difference between letter and 'a' to give x pos in matrix (might not work)
@@ -84,9 +118,9 @@ export function infoUpdate(info: number[], guess: String){
                 for(let j = 0; j < 26; j++){
                     //
                     if(j == letter){
-                        knownInfo[i][j] = 1
+                        output[i][j] = 1
                     }else { 
-                        knownInfo[i][j] = 2
+                        output[i][j] = 2
                     }
                 }
                 break
@@ -94,25 +128,42 @@ export function infoUpdate(info: number[], guess: String){
             case 2:
                 // loop down column setting 2 for everything
                 for(let j = 0; j < 5; j++)
-                    knownInfo[j][letter] = 2
+                    output[j][letter] = 2
                 break
             // letter is in word but not in this position
             case 3:
                 // check if letter is already 2(green guess previously)
-                if( knownInfo[i][letter] == 2)
+                if( output[i][letter] == 2)
                     break
                 // set guessed letter to 2 and all other info-less in column to 3
-                for(let j = 0; j < 4; j++){
+                for(let j = 0; j < 5; j++){
                     if(j == i){
-                        knownInfo[j][letter] = 2
-                    } else if(knownInfo[j][letter] == 0){
-                        knownInfo[j][letter] = 3
+                        output[j][letter] = 2
+                    } else if(output[j][letter] == 0){
+                        output[j][letter] = 3
                     }
                 }
-                neededLetters.push(letter)
+                output[5][letter] = 1
                 break
         }
-        // call helper to remove immpossible words
-        removeWords();   
+        
     }
+    return output as number[][] 
+}
+
+export function generateGuess(): String{
+    let minid = -1
+    let min = dictionary.length
+    for(let i = 0; i < dictionary.length;i++){
+        let posWords = 0
+        for(let j = 0; j < possibleWords.length;j++){
+            posWords += removeWords(possibleWords,infoUpdate(toArray(dictionary[i],possibleWords[j]),dictionary[i],knownInfo)).length
+        }
+        if(posWords < min){
+            min = posWords
+            minid = i
+        }
+    }
+
+    return dictionary[minid]
 }
